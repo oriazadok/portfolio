7ebb2534a3220179f990a6b8dde301c5f0d47210
oriazadok <oriazadok@gmail.com>
2024-08-11 19:01:04 +0300

    set mailing service

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,28 @@
-import '../styles/contact.css'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';  // Ensure this line is correct
+import '../styles/contact.css';
 
 const Contact = () => {
-  const sendEmail = () => {
-    const subject = 'Regarding Your Message';
-    const body = 'Hello Oria,\n\nI have a message for you:\n\n';
 
-    // Compose the email address (replace with your actual email address)
-    const email = 'oriazadok@gmail.com';
+  const form = useRef();
 
-    // Create a "mailto" link with the subject and body
-    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
-      subject
-    )}&body=${encodeURIComponent(body)}`;
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    // Open the user's default email client with the pre-filled email
-    window.location.href = mailtoLink;
+    emailjs
+      .sendForm('service_aatxqnk', 'template_gn2rgna', form.current, {
+              publicKey: 'Q0N48bNaqZbgS-rQk',
+            })
+      .then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+          alert('Message sent successfully!');
+        },
+        (err) => {
+          console.error('FAILED...', err);
+          alert('Failed to send message, please try again.');
+        }
+      );
   };
 
   return (
@@ -22,12 +30,26 @@ const Contact = () => {
       <div className="contact-content">
         <h2>Contact Me</h2>
         <p>Feel free to get in touch with me if you have any questions or inquiries.</p>
-        <div className="contact-form">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Your Email" />
-          <textarea placeholder="Your Message"></textarea>
-          <button onClick={sendEmail}>Send Message</button>
-        </div>
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_name"
+            placeholder="Your Name"
+            required
+          />
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Your Email"
+            required
+          />
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit">Send Message</button>
+        </form>
       </div>
     </section>
   );
